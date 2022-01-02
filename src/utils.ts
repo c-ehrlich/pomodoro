3587c7ac7c93dd67c078843b7f66bcbbb3ee66ff
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-02 19:41:00 +0100

    create util functions

--- src/utils.ts
+++ src/utils.ts
@@ -14,11 +14,11 @@ export const getRemainingTime = (endTime: number): TimeInterface => {
 
 /**
  * @function minutesToMilliseconds
- * multiplies a number by 60000 
+ * multiplies a number by 60000
  */
 export const minutesToMilliseconds = (minutes: number): number => {
   return minutes * 60000;
-}
+};
 
 /**
  * @function convertMillisecondTimeToObject
@@ -30,10 +30,46 @@ export const minutesToMilliseconds = (minutes: number): number => {
  * @returns
  */
 export const convertMillisecondTimeToObject = (time: number): TimeInterface => {
+  console.log("time: ", time);
   return {
-    minutes: 0,
-    seconds: 0,
+    minutes: Math.floor(time / 60000),
+    seconds: Math.floor((time % 60000) / 1000),
   };
 };
 
+/**
+ * @function convertObjectTimeToMilliseconds
+ *
+ * @param time
+ * @returns
+ */
+export const convertObjectTimeToMilliseconds = (
+  time: TimeInterface
+): number => {
+  return time.minutes * 60000 + time.seconds * 1000;
+};
 
+export const handleStartTimer = ({
+  initial,
+  sessionLength,
+  breakLength,
+  remainingTime,
+}: {
+  initial: boolean;
+  sessionLength: number;
+  breakLength: number;
+  remainingTime: TimeInterface;
+}) => {
+  let endTime: number;
+  if (initial) {
+    endTime = Date.now() + minutesToMilliseconds(sessionLength);
+  } else {
+    endTime = Date.now() + convertObjectTimeToMilliseconds(remainingTime);
+  }
+
+  return {
+    endTime: endTime,
+    paused: false,
+    initial: false,
+  }
+};
