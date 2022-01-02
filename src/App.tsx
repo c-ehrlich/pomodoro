8b655a3cc7797894449a0a7e3b611ccc33edef41
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-02 19:41:38 +0100

    add bottom row buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import useStore from "./store";
 import styled from "styled-components";
 import Container from "./components/Container";
 import SetDuration from "./components/SetDuration";
 import Timer from "./components/Timer";
 import ButtonRow from "./components/ButtonRow";
-import ButtonInput from "./components/ButtonInput";
+import BottomRowButton from "./components/BottomRowButton";
+import { faPause, faPlay, faRedoAlt } from "@fortawesome/free-solid-svg-icons";
 
 const StyledApp = styled.div`
   width: 100vw;
@@ -15,14 +17,23 @@ const StyledApp = styled.div`
 `;
 
 function App() {
+  const resetState = useStore((state) => state.resetState);
+  const startTimer = useStore((state) => state.startTimer);
+  const pauseTimer = useStore((state) => state.pauseTimer);
+  const paused = useStore((state) => state.paused);
+
   return (
     <StyledApp className="App">
       <Container>
         <Timer />
         <SetDuration />
         <ButtonRow>
-          <ButtonInput />
-          <div>hello</div>
+          {paused ? (
+            <BottomRowButton icon={faPlay} onClick={startTimer} passdownId="start_stop" />
+          ) : (
+            <BottomRowButton icon={faPause} onClick={pauseTimer} passdownId="start_stop" />
+          )}
+          <BottomRowButton icon={faRedoAlt} onClick={resetState} passdownId="reset" />
         </ButtonRow>
       </Container>
     </StyledApp>
