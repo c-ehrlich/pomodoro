2a544fa7cbaba2e340cfc44107d978af114090f5
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-02 19:41:30 +0100

    create initial timer functionality

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import { useEffect, useRef, useState, useMemo } from "react";
+import useStore from "../store";
+import { convertMillisecondTimeToObject, convertObjectTimeToMilliseconds, getRemainingTime } from "../utils";
 
-interface Props {
-  
-}
+interface Props {}
 
 const Timer = (props: Props) => {
+  // const endTime = useStore(state => state.endTime);
+  let timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const remainingTime = useStore((state) => state.remainingTime);
+  const setRemainingTimeFromNumber = useStore((state) => state.setRemainingTimeFromNumber);
+  const sessionLength = useStore((state) => state.sessionLength);
+  const endTime = useStore((state) => state.endTime);
+  const paused = useStore((state) => state.paused);
+  const initial = useStore((state) => state.initial);
+  const [now, setNow] = useState<number>(0);
+
+  const countdown = (): void => {
+    console.log(Date.now());
+    setNow(Date.now());
+    setRemainingTimeFromNumber(endTime - Date.now());
+    timerRef.current = setTimeout(countdown, 1000);
+  };
+
+  const getTimeLeft = useMemo((): string => {
+    if (initial) {
+      return `${sessionLength}:00`;
+    } else if (paused) {
+      return `${remainingTime.minutes}:${remainingTime.seconds}`;
+    }
+    const timeLeft = getRemainingTime(endTime);
+    return `${timeLeft.minutes}:${timeLeft.seconds}`;
+  }, [initial, paused, endTime, sessionLength, remainingTime]);
+
+  useEffect(() => {
+    if (paused) {
+      clearTimeout(timerRef.current as ReturnType<typeof setTimeout>);
+    } else {
+      timerRef.current = setTimeout(countdown, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerRef.current as ReturnType<typeof setTimeout>);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [paused]);
+
   return (
     <div>
       <div id="timer-label">Session</div>
-      <div id="time-left">25:00</div>
+      <div id="time-left">{getTimeLeft}</div>
+      <div>{now} / {endTime}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Timer
+export default Timer;
